16084eeb31e3ce49bec4e5fecad69547388da324
Joshua <hasianjoshua@gmail.com>
2021-03-21 21:09:16 -0400

    Dependencies Manager

--- install.py
+++ install.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python3
+
 from sys import argv
 from os import system
+import requests
 
 # Get the arguments
 modules = argv[1:]
@@ -10,18 +13,16 @@ def prettify(modules):
     last_found_ending = 0
 
     for i in range(ending_amount):
-        # ! TODO : GET THE ENDINGS AND ADD NEWLINES AFTER EVERY SINGLE ONE OF THEM
-        # ! TODO : GET THE ENDINGS AND ADD NEWLINES AFTER EVERY SINGLE ONE OF THEM
-        # ! TODO : GET THE ENDINGS AND ADD NEWLINES AFTER EVERY SINGLE ONE OF THEM
-        # ! TODO : GET THE ENDINGS AND ADD NEWLINES AFTER EVERY SINGLE ONE OF THEM
-        # ! TODO : GET THE ENDINGS AND ADD NEWLINES AFTER EVERY SINGLE ONE OF THEM
-        # ! TODO : GET THE ENDINGS AND ADD NEWLINES AFTER EVERY SINGLE ONE OF THEM
-        # ! TODO : GET THE ENDINGS AND ADD NEWLINES AFTER EVERY SINGLE ONE OF THEM
-        # ! TODO : GET THE ENDINGS AND ADD NEWLINES AFTER EVERY SINGLE ONE OF THEM
-        # ! TODO : GET THE ENDINGS AND ADD NEWLINES AFTER EVERY SINGLE ONE OF THEM
-        current_ending_idx = ending_amount.index('[end]', last_found_ending)
+        
+        current_ending_idx = modules.index('[end]', last_found_ending + 2)
+        
+        if (len(modules) <= current_ending_idx + 1 or modules[current_ending_idx + 1] != ''):
 
-        last_found_ending = current_ending_idx
+            last_found_ending = current_ending_idx 
+            modules.insert(current_ending_idx + 1, '')
+
+    return modules
+    
 
 def write_modules():
     print('Installing modules and adding it to dependencies.toml')
@@ -38,14 +39,48 @@ def write_modules():
     # Get the modules_end
     ending_idx = content.index('[end]', start_idx)
 
-    # Insert the new module
-    content.insert(ending_idx, 'colorama')
+    modules_area = content[start_idx + 1 : ending_idx]
 
+    # Loop through every modules
+    for module in modules:
+        print()
+        print(f'Checking pip if \'{ module }\' is a real package ...')
+
+        # Go to pypi's api and check if the module exist
+        response = requests.get(f'https://pypi.python.org/pypi/{module}/json')
+        
+        # If we the status code we got back is not 200
+        if response.status_code != 200:
+            print(f'The module \'{ module }\' does not exist in pip\'s database.')
+            print('Make sure you check if your spelling is correct!')
+            continue
+
+        print('Module exist, continuing process ...')
+
+        # If the module exist
+        if (module in modules_area):
+            # Inform user
+            print(f'The module \'{ module }\' already exist in dependencies, ignoring...')
+            continue
+
+        # If the module doesn't exist in the dependencies
+        else:
+            content.insert(ending_idx, module)
+
+    # Prettify the string
     prettified = prettify(content)
+
+    # Join the string together
+    formatted = '\n'.join(prettified)
+
+    # file output
+    fout = open('./config.toml', 'w')
+
+    fout.write(formatted)
+
 
 
 write_modules()
 
-# fout = open('./config.toml', 'w')
 
 print(f'python -m pip install {" ".join(modules)}')
