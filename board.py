285787c803a888d5020b43882aac67de61d386cc
Joshua Pelealu <hasianjoshua@gmail.com>
2021-03-24 12:50:24 -0400

    Print everything at once

--- board.py
+++ board.py
@@ -1,12 +1,11 @@
-from getarrow import getarrow
-from utils.getarrow import getarrow
+from utils.getcontrols import getcontrols
 from os import system, name as sysname
 from utils.drawingblocks import DrawingBlocks
 from colorama import init
 from termcolor import colored
 from time import sleep
 from utils.getch import getch, _Getch
-
+from print_at import print_at
 
 init()
 
@@ -24,17 +23,14 @@ MIDDLE_CONNECT_RIGHT = None
 FOUR_WAY = None
 MIDDLE_CONNECT_UP = None
 MIDDLE_CONNECT_DOWN = None
-<< << << < HEAD
 D_TOP_LEFT_CORNER = None
 D_TOP_RIGHT_CORNER = None
 D_BOTTOM_LEFT_CORNER = None
 D_BOTTOM_RIGHT_CORNER = None
-== == == =
 D_TOP_LEFT_CORNER = None
 D_TOP_RIGHT_CORNER = None
 D_BOTTOM_LEFT_CORNER = None
 D_BOTTOM_RIGHT_CORNER = None
->>>>>> > pynput
 
 # Dynamically declares all the drawing blocks variables
 for key, val in DrawingBlocks().getAll(return_format='dict').items():
@@ -44,111 +40,138 @@ for key, val in DrawingBlocks().getAll(return_format='dict').items():
 class Board:
     def __init__(self):
         # 7 * 6 Board filled with Whatever is in the string
-        # self.board_arr = [['.'] * 7] * 6
+        self.board_arr = [['.'] * 7] * 6
+
+        self.highlighted = 1
 
         self.selected = 1
+        self.clear()
 
         # For testing purposes
-        self.board_arr = [
-            ['.', '.', '.', '.', '.', '.', '.'],
-            ['.', 'R', 'R', 'B', '.', '.', '.'],
-            ['.', 'B', 'R', 'B', '.', '.', '.'],
-            ['.', 'B', 'B', 'R', '.', '.', '.'],
-            ['.', 'R', 'R', 'B', 'R', 'B', '.'],
-            ['.', 'B', 'R', 'B', 'R', 'R', 'B']
-        ]
+        # self.board_arr = [
+        #     ['.', '.', '.', '.', '.', '.', '.'],
+        #     ['.', 'R', 'R', 'B', '.', '.', '.'],
+        #     ['.', 'B', 'B', 'B', '.', '.', '.'],
+        #     ['.', 'B', 'B', 'R', '.', '.', '.'],
+        #     ['.', 'R', 'R', 'B', 'R', 'B', '.'],
+        #     ['.', 'B', 'R', 'B', 'R', 'R', 'B']
+        # ]
 
     def clear(self):
         if sysname == 'nt':
             system('cls')
         else:
             system('clear')
+    # ! Implement a move function that adds in blocks into the array
+    # def move(self, col):
 
     def draw_board(self):
-        self.clear()
-        print()
-        print(f'{TOP_LEFT_CORNER}' + f'   {MIDDLE_CONNECT_DOWN}' * 6 + f'   {TOP_RIGHT_CORNER}', end="")
+        # self.clear()
 
-        print()
+        board_str = [['\n']]
+
+        # print()
+        board_str.append([f'{TOP_LEFT_CORNER}' + f'   {MIDDLE_CONNECT_DOWN}' * 6 + f'   {TOP_RIGHT_CORNER}'])
+
+        # print()
 
         seperator = f'{MIDDLE_CONNECT_RIGHT}   ' + (f'{FOUR_WAY}   ') * 6 + MIDDLE_CONNECT_LEFT
 
         for i, row in enumerate(self.board_arr):
+            board_row_str = []
             for j in row:
-                print(f'{VERTICAL_LINE} {j} ', end="")
-                # print('h')
+                board_row_str.append(
+                    f'{VERTICAL_LINE} {colored("O", "red") if j == "R" else colored("O", "blue") if j == "B" else j} ')
 
-            print(VERTICAL_LINE + ' ' * 10, end="")
+            board_row_str.append(VERTICAL_LINE + ' ' * 10)
             info_box_width = 35
 
             if (i == 1):
-                print(f'{TOP_LEFT_CORNER}{HORIZONTAL_LINE * info_box_width}{TOP_RIGHT_CORNER}', end="\n")
-                print(seperator, end="")
-                print(' ' * 10 + VERTICAL_LINE + ' ' * info_box_width + VERTICAL_LINE)
+                board_row_str.append(f'{TOP_LEFT_CORNER}{HORIZONTAL_LINE * info_box_width}{TOP_RIGHT_CORNER}\n')
+                board_row_str.append(seperator)
+                board_row_str.append(' ' * 10 + VERTICAL_LINE + ' ' * info_box_width + VERTICAL_LINE)
 
             elif (2 <= i < 4):
                 if (i == 2):
-                    print(VERTICAL_LINE + f'{"SOMEBODY CHOSE X or O":^35}' + VERTICAL_LINE)
-                    print(seperator, end="")
-                    print(' ' * 10 + VERTICAL_LINE + ' ' * info_box_width + VERTICAL_LINE)
+                    board_row_str.append(VERTICAL_LINE + f'{"SOMEBODY CHOSE X or O":^35}' + VERTICAL_LINE + '\n')
+                    board_row_str.append(seperator)
+                    board_row_str.append(' ' * 10 + VERTICAL_LINE + ' ' * info_box_width + VERTICAL_LINE)
 
                 elif (i == 3):
-                    print(VERTICAL_LINE + f'{"It is somebody turn":^35}' + VERTICAL_LINE)
-                    print(seperator, end="")
-                    print(' ' * 10 + VERTICAL_LINE + ' ' * info_box_width + VERTICAL_LINE)
+                    board_row_str.append(VERTICAL_LINE + f'{"It is somebody turn":^35}' + VERTICAL_LINE + '\n')
+                    board_row_str.append(seperator)
+                    board_row_str.append(' ' * 10 + VERTICAL_LINE + ' ' * info_box_width + VERTICAL_LINE)
                 else:
-                    print(VERTICAL_LINE + ' ' * info_box_width + VERTICAL_LINE)
-                    print(seperator, end="")
-                    print(' ' * 10 + VERTICAL_LINE + ' ' * info_box_width + VERTICAL_LINE)
+                    board_row_str.append(VERTICAL_LINE + ' ' * info_box_width + VERTICAL_LINE)
+                    board_row_str.append(seperator)
+                    board_row_str.append(' ' * 10 + VERTICAL_LINE + ' ' * info_box_width + VERTICAL_LINE)
 
             elif (i == 4):
-                print(f'{BOTTOM_LEFT_CORNER}{HORIZONTAL_LINE * info_box_width}{BOTTOM_RIGHT_CORNER}', end="\n")
-                print(seperator)
+                board_row_str.append(f'{BOTTOM_LEFT_CORNER}{HORIZONTAL_LINE * info_box_width}{BOTTOM_RIGHT_CORNER}\n')
+                board_row_str.append(seperator)
 
             else:
+                board_row_str.append(f'\n{MIDDLE_CONNECT_RIGHT}   ' + (f'{FOUR_WAY}   ') * 6 + MIDDLE_CONNECT_LEFT)
 
-                print()
-                print(f'{MIDDLE_CONNECT_RIGHT}   ' + (f'{FOUR_WAY}   ') * 6 + MIDDLE_CONNECT_LEFT)
+            board_str.append(board_row_str)
 
         # Last line
-        print(f'{BOTTOM_LEFT_CORNER}' + (f'{HORIZONTAL_LINE * 3}' + MIDDLE_CONNECT_UP)
-              * 6 + f'{HORIZONTAL_LINE * 3}{BOTTOM_RIGHT_CORNER}')
+        board_str.append([f'{BOTTOM_LEFT_CORNER}' + (f'{HORIZONTAL_LINE * 3}' + MIDDLE_CONNECT_UP)
+                          * 6 + f'{HORIZONTAL_LINE * 3}{BOTTOM_RIGHT_CORNER}'])
 
         # selected = 2
 
         # Top Selector
+        _top = []
         for i in range(7):
-            if (i + 1 == self.selected):
-                print(f'{D_TOP_LEFT_CORNER}   {D_TOP_RIGHT_CORNER}', end="")
+            if (i + 1 == self.highlighted):
+                _top.append(f'{D_TOP_LEFT_CORNER}   {D_TOP_RIGHT_CORNER}')
             else:
-                print('    ', end="")
+                _top.append('    ')
 
-        print()
+        board_str.append(_top)
+        # print()
 
         # Numbers
+        _nums = []
         for i in range(7):
-            print(f'  {i + 1} ', end='')
+            _nums.append(f'  {i + 1} ')
 
-        print()
+        board_str.append(_nums)
+        # print()
 
         # Bottom Selector
+        _bottom = []
         for i in range(7):
-            if (i + 1 == self.selected):
-                print(f'{D_BOTTOM_LEFT_CORNER}   {D_BOTTOM_RIGHT_CORNER}', end="")
+            if (i + 1 == self.highlighted):
+                _bottom.append(f'{D_BOTTOM_LEFT_CORNER}   {D_BOTTOM_RIGHT_CORNER}')
             else:
-                print('    ', end="")
+                _bottom.append('    ')
+        board_str.append(_bottom)
+
+        # Print the whole thing as a string
+        print_at(1, 1, '\n'.join([''.join(row) for row in board_str]))
 
 
+# ! Selecting which column of the number
 x = Board()
 x.draw_board()
 
 while(True):
-    key = getarrow()
+    key = getcontrols()
 
-    if (key == 'L_KEY' and x.selected > 1):
-        x.selected -= 1
+    if (key == 'L_KEY' and x.highlighted > 1):
+        x.highlighted -= 1
         x.draw_board()
+        continue
 
-    if (key == 'R_KEY' and x.selected < 7):
-        x.selected += 1
+    if (key == 'R_KEY' and x.highlighted < 7):
+        x.highlighted += 1
         x.draw_board()
+        continue
+
+    if (key == 'ENTER_KEY'):
+        x.selected = x.highlighted
+        break
+
+print(f'{x.selected} is selected')
